26ae617b4f200a61344b4ec39ffa7b2bf270d590
Drake-Ash <ashwinv4444@gmail.com>
2017-07-15 19:03:23 +0530

    corrected PHP

--- classFormEval.php
+++ classFormEval.php
@@ -3,12 +3,16 @@ $Qnum = 1;
 $MCI_Values = array(0,0,0,0,0);
 $MCI_Attributes = array('S','F','Cm','D','Ch');
 
+
 while($Qnum<26)
 {
-	if($_POST[''.$Qnum]=='Yes')
+	if(isset($_POST["q".$Qnum]))
+	{
+	if($_POST["q".$Qnum]=="Yes")
 		$MCI_Values[$Qnum%5] = $MCI_Values[$Qnum%5] + 3;
-	elseif($_POST[''.$Qnum]=='No')
+	elseif($_POST["q".$Qnum]=="No")
 		$MCI_Values[$Qnum%5] = $MCI_Values[$Qnum%5] + 1;
+	}
 	else
 		$MCI_Values[$Qnum%5] = $MCI_Values[$Qnum%5] + 2;
 	$Qnum++;
@@ -18,6 +22,5 @@ while($index<5)
 {
 	$MCI_Values[$index] = $MCI_Values[$index]/5;
 	$index++;
-	echo($MCI_Values[$index]);
 }
 ?>
