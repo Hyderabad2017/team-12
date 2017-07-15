ce9c3c18330ae05b98b89de24d04fc2d6242bb06
Drake-Ash <ashwinv4444@gmail.com>
2017-07-15 17:52:20 +0530

    Added code for adding to db for Classroom assessment

--- classFormEval.php
+++ classFormEval.php
@@ -1,11 +1,23 @@
 <?php
-if(isset($_POST['submit']))
+$Qnum = 1;
+$MCI_Values = array(0,0,0,0,0);
+$MCI_Attributes = array('S','F','Cm','D','Ch');
+
+while(Qnum<26)
 {
-	if(!empty($_POST['radio']))
-	{
-		foreach($_POST['radio'] as $_selected)
-		{
-			$row = "INSERT INTO ______ (   ) VALUES ('$_selected')"
-		}
-	}
+	if($_POST[''.$Qnum]=='Yes')
+		$MCI_Values[$Qnum%5] = $MCI_Values[$Qnum%5] + 3;
+	elseif($_POST[''.$Qnum]=='No')
+		$MCI_Values[$Qnum%5] = $MCI_Values[$Qnum%5] + 1;
+	else
+		$MCI_Values[$Qnum%5] = $MCI_Values[$Qnum%5] + 2;
+	$Qnum++;
 }
+$index = 0;
+while($index<5)
+{
+	$MCI_Values[$index] = $MCI_Values[$index]/5;
+	$index++;
+	echo($MCI_Values[$index]);
+}
+?>
