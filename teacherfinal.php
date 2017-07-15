3c982e0c75ef23bdc66e70544c58c018b6d8bd6c
rohan-vannala <vannala91@gmail.com>
2017-07-16 03:03:53 +0530

    final teacher

--- teacherfinal.php
+++ teacherfinal.php
@@ -92,10 +92,9 @@ echo "<tr>";
 echo "<th>id</th><th>name</th><th>dept</th><th>sal</th>";
 echo "</tr>";
 while($row=mysqli_fetch_array($res))
-{
-	$id1=$row['id']; 
+{ 
 	echo "<tr><td>",$row['id'],"</td>";
-	echo "<td><a href="childassess.php?res=$id1">",$row['name'],"</a></td>";
+	echo "<td>",$row['name'],"</a></td>";
 	echo "<td>",$row['dept'],"</td>";
 	echo "<td>",$row['sal'],"</td></tr>";
 }
