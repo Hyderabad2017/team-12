74471d79ee5a023a9f2379b1909e035397670c16
swathi kalaga <raghavi.kalaga@gmail.com>
2017-07-15 21:39:07 +0530

    child assessment

---
+++ childvals.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 15, 2017 at 06:07 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 7.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bookings`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `childvals`
+--
+
+CREATE TABLE `childvals` (
+  `id` varchar(255) NOT NULL,
+  `skill1` int(255) NOT NULL,
+  `skill2` int(255) NOT NULL,
+  `skill3` int(255) NOT NULL,
+  `skill4` int(255) NOT NULL,
+  `skill5` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
