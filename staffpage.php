fafc2b2645d98d3a51adcdc64c5e98efd64f1579
Cherishma <Cherishma>
2017-07-16 00:13:10 +0530

    added details of admin login

--- staffpage.php
+++ staffpage.php
@@ -5,7 +5,11 @@ $uid = $_POST['uname'];
 $pass = $_POST['psw'] ;
 echo $uid;
 echo $pass;
-echo 'sadeswed';
+if(isset($_REQUEST["submit"]))
+{
+//echo 'sadeswed';
+if($_REQUEST["login"]=="teacher")
+{
 $sql="SELECT * FROM teacher WHERE uname='$uid' AND pwd='$pass' ";
 $res = mysqli_query($conn,$sql);
 if(!$row=mysqli_fetch_assoc($res))
@@ -15,7 +19,24 @@ if(!$row=mysqli_fetch_assoc($res))
 else
 {
 	echo "LOGIN SUCCESSFUL";
-	header("Location: google.co.in");
+	header("Location: teacherhome.php");
+}
+}
+
+if($_REQUEST["login"]=="admin")
+{
+$sql="SELECT * FROM admin WHERE uname='$uid' AND pwd='$pass' ";
+$res = mysqli_query($conn,$sql);
+if(!$row=mysqli_fetch_assoc($res))
+{
+	echo "LOGIN FAILED";
+}
+else
+{
+	echo "LOGIN SUCCESSFUL";
+	header("Location: adminhome.php");
+}
+}
 }
 
 ?>
